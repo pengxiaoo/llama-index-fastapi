32c742bd12cba8b58f64a80cdb93308f74f5d01c
pengxiaoo <alexpengxiao@gmail.com>
2023-10-28 11:48:36 +0800

    update for uploading files

--- app/routers/qa.py
+++ app/routers/qa.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
-from app.data.messages.qa import QuestionAnsweringRequest, QuestionAnsweringResponse
+import os
+from fastapi import APIRouter, UploadFile, File
+from app.data.messages.qa import QuestionAnsweringRequest, QuestionAnsweringResponse, \
+    BaseResponseModel
 from app.common.log_util import logger
 from app.common import manager_util
 
@@ -7,6 +9,8 @@ qa_router = APIRouter(
     prefix="/qa",
     tags=["question answering"],
 )
+
+PATH_DOCUMENTS = '../documents'
 
 
 @qa_router.post("/query", response_model=QuestionAnsweringResponse)
@@ -26,7 +30,27 @@ async def answer_question(req: QuestionAnsweringRequest):
 
 
 @qa_router.get("/documents", response_model=QuestionAnsweringResponse)
-async def get_documents_list(req: QuestionAnsweringRequest):
+async def get_documents_list():
     manager = manager_util.get_manager()
     documents = manager.get_documents_list()._getvalue()
     return QuestionAnsweringResponse(data=documents)
+
+
+@qa_router.post("/uploadFile", response_model=BaseResponseModel)
+def upload_file(uploaded_file: UploadFile = File(..., description="files for indexing"), ):
+    manager = manager_util.get_manager()
+    filepath = None
+    try:
+        filename = uploaded_file.filename
+        filepath = os.path.join(PATH_DOCUMENTS, os.path.basename(filename))
+        # save file to local
+        with open(filepath, 'wb') as buffer:
+            buffer.write(uploaded_file.file.read())
+        manager.insert_into_index(filepath, doc_id=filename)
+    except Exception as e:
+        return BaseResponseModel(msg="File uploaded failed: {}".format(str(e)))
+    finally:
+        # cleanup temp file
+        if filepath is not None and os.path.exists(filepath):
+            os.remove(filepath)
+    return BaseResponseModel(msg="File uploaded successfully")
