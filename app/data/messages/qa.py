bf9d5d0e5f1d0723a97ce1acf1ae7ed07299d496
pengxiaoo <alexpengxiao@gmail.com>
2023-11-06 08:35:00 +0800

    update unit tests

--- app/data/messages/qa.py
+++ app/data/messages/qa.py
@@ -7,6 +7,19 @@ from app.data.messages.response import BaseResponseModel
 class QuestionAnsweringRequest(BaseModel):
     question: str = Field(..., description="question to be answered")
 
+    class ConfigDict:
+        json_schema_extra = {
+            "example_golf_relevant_1": {
+                "question": "How to play golf in bad whether?"
+            },
+            "example_golf_relevant_2": {
+                "question": "How much money it will cost if I buy a full set of golf clubs?"
+            },
+            "example_not_relevant": {
+                "question": "What is the capital of China?"
+            },
+        }
+
 
 class QuestionAnsweringResponse(BaseResponseModel):
     data: Optional[Answer] = Field(None, description="answer to the question")
--- app/tests/test_base.py
+++ app/tests/test_base.py
@@ -1,14 +1,27 @@
 import unittest
+import time
 from fastapi.testclient import TestClient
+from app.launch import index_server_main
 from app.main import app
+from app.data.messages.status_code import StatusCode
+from app.data.messages.qa import QuestionAnsweringRequest
 
 
 class BaseTest(unittest.TestCase):
     client = TestClient(app=app)
     ROOT = "/api/v1"
+    ROUTER = "qa"
+    index_process = None
 
     def setUp(self):
-        pass
+        self.index_process = index_server_main()
+        time.sleep(10)
 
     def tearDown(self) -> None:
-        pass
+        self.index_process.terminate()
+
+    def test_ask_questions(self):
+        data = QuestionAnsweringRequest.ConfigDict.json_schema_extra["example_not_relevant"]
+        response = self.client.post(url=f"{self.ROOT}/{self.ROUTER}/query", json=data)
+        json_dict = response.json()
+        self.assertEqual(json_dict["status_code"], StatusCode.SUCCEEDED, f"response: {json_dict}")
